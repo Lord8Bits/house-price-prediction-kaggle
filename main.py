025b4fb816c2a396a78c7fab3d845e018411c5fa
Aymane <aymane.larhrissi1@gmail.com>
2025-11-05 21:15:47 +0100

    Better code and logic for feature selection by testing the mae values for each newly added feature and keeping the ones with better mae

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data_path = 'input/home-data-for-ml-course/train.csv'
+home_data = pd.read_csv(data_path)
+home_data = home_data.loc[:, home_data.columns != 'Id']
+home_data = pd.get_dummies(home_data)
 
+original_features = home_data.columns
+print(f"Original features shape: {home_data.shape}")
+print(f"Number of features: {len(original_features)}")
+print(home_data.head())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+selected_features = []
+y = home_data.SalePrice # Sale prices
+def split_data(features):
+    global y, home_data
+    X = home_data[features]
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1) # Create training data and validation data
+    home_data = pd.get_dummies(home_data) # Convert any string values into numerical data
+    home_model.fit(train_X, train_y) # Train the model with the training data
+    return val_X, val_y
 
+def predict(input_columns, output):
+    global home_model
+    pred = home_model.predict(input_columns) # Make the predictions
+    mean_error = mean_absolute_error(output, input_columns) # Calculate the Mean absolute error
+    return pred, mean_error
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+home_model = RandomForestRegressor(random_state=1) # Current ML model
+previous_mae = 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for feature in original_features:
+    if feature not in selected_features: # We append a new feature to a list to test the new MAE
+        selected_features.append(feature)
+        val_X, val_y = split_data(selected_features)
+        predictions, mae = predict(val_X, val_y)
+        print(f"MAE for {feature}: {mae}") # Check current feature tested
+
+        if not previous_mae: previous_mae = mae
+        if min(mae, previous_mae) == mae:
+            previous_mae = mae
+            print(f"New MAE: {mae}")
+        else:
+            selected_features.remove(feature)
+
+# Write the important features to a file:
+home_data = home_data[selected_features]
+home_data.to_csv("one-to-one.csv", index=False)
